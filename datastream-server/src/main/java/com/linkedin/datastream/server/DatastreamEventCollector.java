e48e4611b168f90c41889ffb8006ead6dd221c86
Peng Du <pdu@linkedin.com>
2015-10-01 16:37:59 -0700

    Add Datastream event collector factory
    Datastream event factory is created to enable the actual collector
    used by Datastream to be configurable. The factory looks for a property
    in the configuration properties and instantiate collectors dynamically
    using Java reflection.

--- datastream-server/src/main/java/com/linkedin/datastream/server/DatastreamEventCollector.java
+++ datastream-server/src/main/java/com/linkedin/datastream/server/DatastreamEventCollector.java
@@ -1,9 +1,16 @@
 package com.linkedin.datastream.server;
 
 /**
- * DatastreamEventCollector is the interface for Connector to send
- * Datastream events on the Datastream Kafka cluster.
+ * Interface for Connector to send Datastream events over Kafka.
+ *
+ * NOTE: currently, each collector is expected to define a constructor
+ * in the form of (Datastream datastream, VerifiableProperties config),
+ * please refer to {@link DatastreamEventCollectorFactory}.
  */
 public interface DatastreamEventCollector {
+  /**
+   * Send a DatastreamEvent on a Kafka cluster
+   * @param record datastream event envelope
+   */
   void send(DatastreamEventRecord record);
 }
