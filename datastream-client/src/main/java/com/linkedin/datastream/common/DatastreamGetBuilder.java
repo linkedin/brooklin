ae29a182662ada010a4203765447faa8c32f48b1
Srinivasulu Punuru <spunuru@linkedin.com>
2015-10-13 18:51:59 -0700

    Generate restli idl files and client builders

---
+++ datastream-client/src/main/java/com/linkedin/datastream/common/DatastreamGetBuilder.java
@@ -0,0 +1,19 @@
+
+package com.linkedin.datastream.common;
+
+import javax.annotation.Generated;
+import com.linkedin.restli.client.RestliRequestOptions;
+import com.linkedin.restli.client.base.GetRequestBuilderBase;
+import com.linkedin.restli.common.ResourceSpec;
+
+@Generated(value = "com.linkedin.restli.tools.clientgen.RestRequestBuilderGenerator", comments = "LinkedIn Request Builder", date = "Tue Oct 13 18:42:03 PDT 2015")
+public class DatastreamGetBuilder
+    extends GetRequestBuilderBase<String, Datastream, DatastreamGetBuilder>
+{
+
+
+    public DatastreamGetBuilder(String baseUriTemplate, ResourceSpec resourceSpec, RestliRequestOptions requestOptions) {
+        super(baseUriTemplate, Datastream.class, resourceSpec, requestOptions);
+    }
+
+}
