6702ef78af687230c87e7eede0ba38153d3833be
Hai Lu <halu@linkedin.com>
2016-01-06 13:25:18 -0800

    Create base event validator

--- datastream-testcommon/src/test/java/com/linkedin/datastream/testutil/TestEventGenerator.java
+++ datastream-testcommon/src/test/java/com/linkedin/datastream/testutil/TestEventGenerator.java
@@ -1,13 +1,10 @@
 package com.linkedin.datastream.testutil;
 
-import com.linkedin.datastream.common.Datastream;
 import com.linkedin.datastream.common.DatastreamEvent;
-
 import com.linkedin.datastream.testutil.event.generator.DatastreamEventGenerator;
-import java.lang.CharSequence;
-import java.lang.Object;
-import java.lang.Override;
-import java.lang.String;
+import com.linkedin.datastream.testutil.event.validator.GenericEventValidator;
+import java.io.IOException;
+import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testng.Assert;
@@ -15,18 +12,13 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import java.io.IOException;
-import java.nio.ByteBuffer;
-import java.nio.file.Paths;
-import java.util.HashMap;
-import java.util.List;
-
 
 public class TestEventGenerator {
   private static Logger LOG = LoggerFactory.getLogger(TestEventGenerator.class);
 
   @BeforeMethod
-  public void setup() throws IOException {
+  public void setup()
+      throws IOException {
     LOG.info("Test EventGenerator class");
   }
 
@@ -35,35 +27,40 @@ public class TestEventGenerator {
     LOG.info("Done testing EventGenerator class");
   }
 
-  private int _numEvents = 10;
-  private int _percentageUpdates = 20;
-  private int _percentageDeletes = 20;
-
   @Test
-  public void testDatastreamEvents() throws Exception {
+  public void testDatastreamEvents()
+      throws Exception {
+    int numEvents = 10;
+    int percentageUpdates = 20;
+    int percentageDeletes = 20;
     // set up
-    String schema =
-        "{" + "   \"name\" : \"TestSchema\"," + "   \"version\": 1," + "   \"type\" : \"record\","
-            + "   \"namespace\" : \"com.linkedin.datastream.test.TestDatastreamEvents\"," + "   \"fields\" : [ "
-            + "   {" + "     \"name\" : \"company\"," + "     \"type\" : \"string\"" + "   }, {"
-            + "     \"name\" : \"ceo\"," + "     \"type\" : \"string\"" + "   }, {" + "     \"name\" : \"taxId\","
-            + "     \"type\" : \"long\"" + "   } ]" + " }";
+    String schema = "{" + "   \"name\" : \"TestSchema\"," + "   \"version\": 1," + "   \"type\" : \"record\","
+        + "   \"namespace\" : \"com.linkedin.datastream.test.TestDatastreamEvents\"," + "   \"fields\" : [ " + "   {"
+        + "     \"name\" : \"company\"," + "     \"type\" : \"string\"" + "   }, {" + "     \"name\" : \"ceo\","
+        + "     \"type\" : \"string\"" + "   }, {" + "     \"name\" : \"taxId\"," + "     \"type\" : \"long\""
+        + "   } ]" + " }";
 
     DatastreamEventGenerator eeg = new DatastreamEventGenerator(schema);
-    eeg.setPercentageUpdates(_percentageUpdates);
-    eeg.setPercentageDeletes(_percentageDeletes);
+    eeg.setPercentageUpdates(percentageUpdates);
+    eeg.setPercentageDeletes(percentageDeletes);
 
     // generate events
-    List<Object> eventList = eeg.generateGenericEventList(_numEvents); // todo this should be generateEventList
+    List<Object> eventList = eeg.generateGenericEventList(numEvents); // todo this should be generateEventList
+    List<Object> eventList2 = eeg.generateGenericEventList(numEvents);
+
+    Assert.assertTrue(GenericEventValidator.validateGenericEventList(eventList, eventList),
+        "Same event lists should pass the event validation");
+    Assert.assertFalse(GenericEventValidator.validateGenericEventList(eventList, eventList2),
+        "Different event lists should fail the event validation");
 
     // verify the events
     int numUpdates = 0;
     int numDeletes = 0;
-    Assert.assertEquals(_numEvents, eventList.size());
+    Assert.assertEquals(numEvents, eventList.size());
     for (Object obj : eventList) {
       @SuppressWarnings("unchecked")
       // just map
-      DatastreamEvent espEvent = (DatastreamEvent) obj;
+          DatastreamEvent espEvent = (DatastreamEvent) obj;
       CharSequence op = espEvent.metadata.get("__OpCode");
       if (op.equals("UPDATE")) {
         ++numUpdates;
@@ -71,7 +68,7 @@ public class TestEventGenerator {
         ++numDeletes;
       }
     }
-    Assert.assertEquals(numUpdates, _numEvents * _percentageUpdates / 100);
-    Assert.assertEquals(numDeletes, _numEvents * _percentageDeletes / 100);
+    Assert.assertEquals(numUpdates, numEvents * percentageUpdates / 100);
+    Assert.assertEquals(numDeletes, numEvents * percentageDeletes / 100);
   }
 }
