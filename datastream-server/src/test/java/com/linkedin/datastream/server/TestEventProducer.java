e7d4a172cd5d2d8b3b65020b9726367c5af64c68
Peng Du <pdu@linkedin.com>
2016-01-28 18:23:35 -0800

    Remove usage of synchronized and add updateTasks support
    TransportProvider (KafkaProducer) send() is thread-safe and we are
    sharing the same EventProducer/Transport among all workers of the same
    destination (for different partitions). Making send() synchronized
    unnecessarily limits the throughput of connector workers. send() only
    needs to be synchronized with flush so this is the classic read-write
    lock scenario. Given such observation, this change replaces all
    synchronized usages with finer grained read-write lock.
    
    Also, added updateTask capability to EventProducer to allow dynamically
    update the task set the producer needs to serve. This is because event
    producer need to survive and be reused across mutliple task assignments.
    Such that we event producer pool would call updateTasks to let producer
    know about the new assignment and make adjustments to internal structures.
    
    Lastly, revised the way how EventProducerPool creates and reuses event
    producers for new tasks so that it can call updateTasks on existing event
    producers.

--- datastream-server/src/test/java/com/linkedin/datastream/server/TestEventProducer.java
+++ datastream-server/src/test/java/com/linkedin/datastream/server/TestEventProducer.java
@@ -6,10 +6,12 @@ import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 import java.util.Random;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
 
-import org.codehaus.jackson.type.TypeReference;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import org.codehaus.jackson.type.TypeReference;
 
 import com.linkedin.data.template.StringMap;
 import com.linkedin.datastream.common.Datastream;
@@ -19,7 +21,6 @@ import com.linkedin.datastream.common.DatastreamException;
 import com.linkedin.datastream.common.DatastreamSource;
 import com.linkedin.datastream.common.JsonUtils;
 import com.linkedin.datastream.common.PollUtils;
-import com.linkedin.datastream.server.api.schemaregistry.SchemaRegistryProvider;
 import com.linkedin.datastream.server.api.transport.TransportException;
 import com.linkedin.datastream.server.api.transport.TransportProvider;
 import com.linkedin.datastream.server.providers.CheckpointProvider;
@@ -27,20 +28,23 @@ import com.linkedin.datastream.testutil.InMemoryCheckpointProvider;
 
 import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.anyMap;
+import static org.mockito.Matchers.anyObject;
+import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.internal.verification.VerificationModeFactory.times;
 
 
-public class TestDatastreamEventProducer {
+public class TestEventProducer {
   private ArrayList<DatastreamTask> _tasks;
   private EventProducer _producer;
   private Datastream _datastream;
   private TransportProvider _transport;
   private CheckpointProvider _cpProvider;
-  private SchemaRegistryProvider _schemaRegistryProvider;
   private Properties _config;
+  private AtomicInteger _inSend = new AtomicInteger(0);
+  private AtomicBoolean _inFlush = new AtomicBoolean(false);
 
   private Datastream createDatastream() {
     Datastream datastream = new Datastream();
@@ -61,7 +65,7 @@ public class TestDatastreamEventProducer {
 
   private int _eventSeed;
 
-  private DatastreamEventRecord createEventRecord(Datastream datastream, DatastreamTask task, Integer partition) {
+  private DatastreamEventRecord createEventRecord(Integer partition) {
     DatastreamEvent event = new DatastreamEvent();
     event.key = null;
     event.payload = null;
@@ -71,6 +75,10 @@ public class TestDatastreamEventProducer {
   }
 
   private void setup(boolean customCheckpointing) {
+    setup(customCheckpointing, false);
+  }
+
+  private void setup(boolean customCheckpointing, boolean checkRace) {
     _datastream = createDatastream();
 
     DatastreamTaskImpl task1 = new DatastreamTaskImpl(_datastream);
@@ -91,9 +99,39 @@ public class TestDatastreamEventProducer {
     _tasks.add(task1);
     _tasks.add(task2);
 
-    //ValidatingTransport transport = new ValidatingTransport();
     _transport = mock(TransportProvider.class);
-    _schemaRegistryProvider = mock(SchemaRegistryProvider.class);
+
+    // Verify transport.send() and transport.flush() is never called simultaneously
+    if (checkRace) {
+      try {
+        doAnswer((invocation) -> {
+          Assert.assertFalse(_inFlush.get());
+          _inSend.incrementAndGet();
+          try {
+            Thread.sleep(1); // mimic send delay
+          } catch (InterruptedException e) {
+            Assert.fail();
+          }
+          _inSend.decrementAndGet();
+          return null;
+        }).when(_transport).send(anyString(), anyObject());
+
+        doAnswer((invocation) -> {
+          Assert.assertEquals(_inSend.get(), 0);
+          _inFlush.set(true);
+          try {
+            Thread.sleep(3); // mimic send delay
+          } catch (InterruptedException e) {
+            Assert.fail();
+          }
+          _inFlush.set(false);
+          return null;
+        }).when(_transport).flush();
+      } catch (TransportException e) {
+        Assert.fail(); // this is impossible
+      }
+    }
+
     if (!customCheckpointing) {
       _cpProvider = new InMemoryCheckpointProvider();
     } else {
@@ -104,9 +142,7 @@ public class TestDatastreamEventProducer {
     _config = new Properties();
     _config.put(EventProducer.CHECKPOINT_PERIOD_MS, "50");
 
-    _producer =
-        new EventProducer(_tasks, _transport, _cpProvider, _config,
-            customCheckpointing);
+    _producer = new EventProducer(_tasks, _transport, _cpProvider, _config, customCheckpointing);
   }
 
   @Test
@@ -119,7 +155,7 @@ public class TestDatastreamEventProducer {
     for (int i = 0; i < 500; i++) {
       task = i % 3 == 0 ? _tasks.get(0) : _tasks.get(1);
       partition = i % 3 == 0 ? 1 + rand.nextInt(2) : 3 + rand.nextInt(2);
-      record = createEventRecord(_datastream, task, partition);
+      record = createEventRecord(partition);
       _producer.send(task, record);
     }
     final boolean[] isCommitCalled = { false };
@@ -164,7 +200,7 @@ public class TestDatastreamEventProducer {
     for (int i = 0; i < 500; i++) {
       task = i % 3 == 0 ? _tasks.get(0) : _tasks.get(1);
       partition = i % 3 == 0 ? 1 + rand.nextInt(2) : 3 + rand.nextInt(2);
-      record = createEventRecord(_datastream, task, partition);
+      record = createEventRecord(partition);
       _producer.send(task, record);
 
       Map<Integer, String> cpMap = taskCpMap.getOrDefault(task, new HashMap<>());
@@ -189,5 +225,114 @@ public class TestDatastreamEventProducer {
     // Expect saved checkpoint to match that of the last event
     Map<DatastreamTask, Map<Integer, String>> checkpointsNew = _producer.getSafeCheckpoints();
     Assert.assertEquals(checkpointsNew, taskCpMap);
+  }
+
+  private void verifyCheckpoints() {
+    int size = _tasks.size();
+    Assert.assertEquals(_producer.getSafeCheckpoints().size(), size);
+    _tasks.forEach(t -> {
+      Assert.assertNotNull(_producer.getSafeCheckpoints().get(t), t.toString());
+      Assert.assertEquals(_producer.getSafeCheckpoints().get(t).keySet(), t.getPartitions(), t.toString());
+    });
+  }
+
+  /**
+   * Helper for tests exercising updateTasks
+   *
+   * 1. add a new task (task3)
+   * 2. remove a task (task3)
+   * 3. add task3 and remove task2
+   *
+   * Validate the checkpoint map after each step.
+   */
+  private void callUpdateTasksAndVerify(int partition) {
+    List<Integer> partitions = new ArrayList<>();
+    partitions.add(partition);
+    partitions.add(partition + 1);
+    DatastreamTaskImpl task3 = new DatastreamTaskImpl(_datastream);
+    task3.setPartitions(partitions);
+    _tasks.add(task3);
+    _producer.updateTasks(_tasks);
+    verifyCheckpoints();
+
+    // Remove a task
+    _tasks.remove(2);
+    _producer.updateTasks(_tasks);
+    verifyCheckpoints();
+
+    // Add and remove a task
+    _tasks.remove(1);
+    _tasks.add(task3);
+    _producer.updateTasks(_tasks);
+    verifyCheckpoints();
+  }
+
+  /**
+   * Basic test case of update tasks
+   */
+  @Test
+  public void testUpdateTasks() {
+    setup(false);
+
+    verifyCheckpoints();
+    callUpdateTasksAndVerify(5);
+  }
+
+  /**
+   * Using three threads doing repeated send/flush/updateTasks and check for race conditions.
+   */
+  @Test
+  public void testSendFlushUpdateTaskStress() {
+    setup(false, true);
+
+    // Mimic a connector constantly calls send/flush
+    final boolean stopHandler[] = { false };
+    Thread sender = new Thread(() -> {
+      DatastreamTask task = _tasks.get(0);
+      while (!stopHandler[0]) {
+        try {
+          Thread.sleep(5);
+        } catch (InterruptedException e) {
+          break;
+        }
+
+        _producer.send(task, createEventRecord(1));
+      }
+    });
+    sender.start();
+
+    Thread flusher = new Thread(() -> {
+      DatastreamTask task = _tasks.get(0);
+      while (!stopHandler[0]) {
+        try {
+          Thread.sleep(5);
+        } catch (InterruptedException e) {
+          break;
+        }
+
+        _producer.flush();
+      }
+    });
+    flusher.start();
+
+    // Repeatedly update the tasks and check for corruption
+    int iterations = 100;
+    for (int i = 0, step = 0; i < iterations; i++, step += 2) {
+      callUpdateTasksAndVerify(step + 100);
+      try {
+        Thread.sleep(2);
+      } catch (InterruptedException e) {
+        Assert.fail();
+      }
+    }
+
+    stopHandler[0] = true;
+    Assert.assertTrue(PollUtils.poll(() -> !flusher.isAlive(), 50, 2000));
+
+    // Remove all tasks
+    _tasks.clear();
+    _producer.updateTasks(_tasks);
+    verifyCheckpoints();
+
   }
 }
