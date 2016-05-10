27da93f832648e5b192d9cef102968e83cd69a54
Peng Du <pdu@linkedin.com>
2016-05-10 10:48:48 -0700

    Add more helper methods into ErrorLogger
    - added non-static methods that use the same logger
    - added methods without inner exception parameter

--- datastream-common/src/main/java/com/linkedin/datastream/common/ErrorLogger.java
+++ datastream-common/src/main/java/com/linkedin/datastream/common/ErrorLogger.java
@@ -1,5 +1,6 @@
 package com.linkedin.datastream.common;
 
+import org.apache.commons.lang.Validate;
 import org.slf4j.Logger;
 
 
@@ -7,6 +8,7 @@ import org.slf4j.Logger;
  * Helper utility methods for error logging.
  */
 public class ErrorLogger {
+  private final Logger _logger;
 
   /**
    * Log error and throw DatastreamRuntimeException with inner exception
@@ -22,5 +24,36 @@ public class ErrorLogger {
       log.error(msg);
       throw new DatastreamRuntimeException(msg);
     }
+  }
+
+  /**
+   * Log error and throw DatastreamRuntimeException with inner exception
+   * @param log logger object
+   * @param msg error message
+   */
+  public static void logAndThrowDatastreamRuntimeException(Logger log, String msg) {
+    logAndThrowDatastreamRuntimeException(log, msg, null);
+  }
+
+  public ErrorLogger(Logger logger) {
+    Validate.notNull(logger, "null logger");
+    _logger = logger;
+  }
+
+  /**
+   * Log error and throw DatastreamRuntimeException with inner exception
+   * @param msg error message
+   * @param t inner exception
+   */
+  public void logAndThrowDatastreamRuntimeException(String msg, Throwable t) {
+    logAndThrowDatastreamRuntimeException(_logger, msg, t);
+  }
+
+  /**
+   * Log error and throw DatastreamRuntimeException with inner exception
+   * @param msg error message
+   */
+  public void logAndThrowDatastreamRuntimeException(String msg) {
+    logAndThrowDatastreamRuntimeException(_logger, msg);
   }
 }
