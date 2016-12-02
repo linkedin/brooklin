08abba6bb3658f160c4a08ccd692fa9ce4051627
Peng Du <pdu@linkedin.com>
2016-12-01 16:47:23 -0800

    utils: add utils to ease executor service shutdown
    Incorporated shutdown recipe from JDK javadoc for proper executor
    service shutdown.

---
+++ datastream-utils/src/test/java/com/linkedin/datastream/common/TestThreadUtils.java
@@ -0,0 +1,63 @@
+package com.linkedin.datastream.common;
+
+import java.time.Duration;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+
+@Test
+public class TestThreadUtils {
+  private void runWorker(CountDownLatch latch, boolean unkillable, ExecutorService executor) {
+    final boolean[] started = { false };
+    executor.submit(() -> {
+      do {
+        try {
+          started[0] = true;
+          latch.await();
+          break;
+        } catch (InterruptedException e) {
+        }
+      } while (unkillable);
+    });
+    // Ensure thread has started
+    Assert.assertTrue(PollUtils.poll(() -> started[0], 100, 30000));
+  }
+
+  @Test
+  public void testCleanShutdown() {
+    Logger logger = LoggerFactory.getLogger("testCleanShutdown");
+    ExecutorService executor = Executors.newSingleThreadExecutor();
+    final CountDownLatch latch = new CountDownLatch(1);
+    runWorker(latch, false, executor);
+
+    Thread t = new Thread(() -> {
+      try {
+        Thread.sleep(200);
+        latch.countDown();
+      } catch (InterruptedException e) {
+        Assert.fail();
+      }
+    });
+    t.start();
+
+    Assert.assertTrue(ThreadUtils.shutdownExecutor(executor, Duration.ofMinutes(1), logger));
+  }
+
+  @Test
+  public void testUncleanShutdown() {
+    Logger logger = LoggerFactory.getLogger("testUncleanShutdown");
+    ExecutorService executor = Executors.newSingleThreadExecutor();
+    final CountDownLatch latch = new CountDownLatch(1);
+    runWorker(latch, true, executor);
+
+    Assert.assertFalse(ThreadUtils.shutdownExecutor(executor, Duration.ofMillis(200), logger));
+    latch.countDown();
+    Assert.assertTrue(ThreadUtils.shutdownExecutor(executor, Duration.ofMinutes(1), logger));
+  }
+}
