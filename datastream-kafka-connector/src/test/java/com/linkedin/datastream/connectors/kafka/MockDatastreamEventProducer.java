4955dc5839c6e0c7656ae4546bfa08ee8588b412
Srinivasulu Punuru <spunuru@linkedin.com>
2017-04-13 15:46:53 -0700

    Enable SeekByTimestamp in kafka connector

--- datastream-kafka-connector/src/test/java/com/linkedin/datastream/connectors/kafka/MockDatastreamEventProducer.java
+++ datastream-kafka-connector/src/test/java/com/linkedin/datastream/connectors/kafka/MockDatastreamEventProducer.java
@@ -4,6 +4,9 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.linkedin.datastream.server.DatastreamEventProducer;
 import com.linkedin.datastream.server.DatastreamProducerRecord;
 import com.linkedin.datastream.server.api.transport.DatastreamRecordMetadata;
@@ -12,12 +15,14 @@ import com.linkedin.datastream.server.api.transport.SendCallback;
 
 public class MockDatastreamEventProducer implements DatastreamEventProducer {
 
+  private static final Logger LOG = LoggerFactory.getLogger(MockDatastreamEventProducer.class);
   private final List<DatastreamProducerRecord> events = Collections.synchronizedList(new ArrayList<>());
   private int numFlushes = 0;
 
   @Override
   public void send(DatastreamProducerRecord event, SendCallback callback) {
     events.add(event);
+    LOG.info("sent event {} , total events {}", event, events.size());
     DatastreamRecordMetadata md = new DatastreamRecordMetadata(event.getCheckpoint(), "mock topic", 666);
     if (callback != null) {
       callback.onCompletion(md, null);
