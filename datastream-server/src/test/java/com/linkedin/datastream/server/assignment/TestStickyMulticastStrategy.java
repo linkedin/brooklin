08077542093ab240f18cead9367a46930f425d6c
Miguel Sanchez <misanche@misanche-ld1.linkedin.biz>
2017-12-15 13:30:12 -0800

    Creating a new Sticky Broadcast Strategy

--- datastream-server/src/test/java/com/linkedin/datastream/server/assignment/TestStickyMulticastStrategy.java
+++ datastream-server/src/test/java/com/linkedin/datastream/server/assignment/TestStickyMulticastStrategy.java
@@ -4,10 +4,13 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
 import java.util.Set;
 
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -26,7 +29,7 @@ import static com.linkedin.datastream.server.assignment.BroadcastStrategyFactory
 import static com.linkedin.datastream.server.assignment.BroadcastStrategyFactory.DEFAULT_MAX_TASKS;
 
 
-public class TestBroadcastStrategy {
+public class TestStickyMulticastStrategy {
 
   private static final Logger LOG = LoggerFactory.getLogger(TestBroadcastStrategy.class.getName());
 
@@ -34,7 +37,7 @@ public class TestBroadcastStrategy {
   public void testBroadcastStrategyCreatesAssignmentAcrossAllInstances() {
     String[] instances = new String[]{"instance1", "instance2", "instance3"};
     List<DatastreamGroup> datastreams = generateDatastreams("ds", 5);
-    BroadcastStrategy strategy = new BroadcastStrategy(DEFAULT_MAX_TASKS);
+    StickyMulticastStrategy strategy = new StickyMulticastStrategy(DEFAULT_MAX_TASKS);
     Map<String, Set<DatastreamTask>> assignment =
         strategy.assign(datastreams, Arrays.asList(instances), new HashMap<>());
     for (String instance : instances) {
@@ -49,7 +52,7 @@ public class TestBroadcastStrategy {
     int maxTasks = 7;
     int expectedTotalTasks = numDatastreams * maxTasks;
     List<DatastreamGroup> datastreams = generateDatastreams("ds", numDatastreams);
-    doTestMaxTasks(new BroadcastStrategy(maxTasks), numInstances, expectedTotalTasks, datastreams);
+    doTestMaxTasks(new StickyMulticastStrategy(maxTasks), numInstances, expectedTotalTasks, datastreams);
   }
 
   @Test
@@ -61,10 +64,11 @@ public class TestBroadcastStrategy {
     datastreams.get(0).getDatastreams().get(0).getMetadata().put(CFG_MAX_TASKS, "18");
 
     int expectedTotalTasks = numDatastreams * maxTasks + (18 - maxTasks);
-    doTestMaxTasks(new BroadcastStrategy(maxTasks), numInstances, expectedTotalTasks, datastreams);
+    doTestMaxTasks(new StickyMulticastStrategy(maxTasks), numInstances, expectedTotalTasks, datastreams);
   }
 
-  private void doTestMaxTasks(BroadcastStrategy strategy, int numInstances, int expectedTotalTasks,
+
+  private void doTestMaxTasks(StickyMulticastStrategy strategy, int numInstances, int expectedTotalTasks,
       List<DatastreamGroup> datastreams) {
     String[] instances = IntStream.range(0, numInstances).mapToObj(x -> "instance" + x).toArray(String[]::new);
     Map<String, Set<DatastreamTask>> assignment =
@@ -76,6 +80,40 @@ public class TestBroadcastStrategy {
       totalTasks += assignment.get(instance).size();
     }
     Assert.assertEquals(totalTasks, expectedTotalTasks);
+  }
+
+  @Test
+  public void testBroadcastStrategyEditDiff() {
+    int numDatastreams = 11;
+    int numInstances = 20;
+    int maxTasks = 7;
+    int expectedTotalTasks = numDatastreams * maxTasks;
+
+    List<DatastreamGroup> datastreams = generateDatastreams("ds", numDatastreams);
+    List<String> instances = IntStream.range(0, numInstances).mapToObj(x -> "instance" + x).collect(Collectors.toList());
+
+    Map<String, Set<DatastreamTask>> assignment1 =
+        new StickyMulticastStrategy(maxTasks).assign(datastreams, instances, new HashMap<>());
+
+    Assert.assertEquals(getFullTaskList(assignment1).size(), expectedTotalTasks);
+    Assert.assertEquals(editDiff(assignment1, new HashMap<>()), expectedTotalTasks);
+
+    // Test instance goes down: "instance
+    Random rnd = new Random();
+    String instanceDown = instances.remove(rnd.nextInt(instances.size()));
+    int numTasksInInstanceDown = assignment1.get(instanceDown).size();
+
+    Map<String, Set<DatastreamTask>> assignment2 =
+        new StickyMulticastStrategy(maxTasks).assign(datastreams, instances, assignment1);
+    Assert.assertTrue(editDiff(assignment1, assignment2) / (numTasksInInstanceDown + 1) <= 2);
+
+    // Test instance coming up
+    instances.add(instanceDown);
+    Map<String, Set<DatastreamTask>> assignment3 =
+        new StickyMulticastStrategy(maxTasks).assign(datastreams, instances, assignment2);
+    int numTasksInNewInstance = assignment3.get(instanceDown).size();
+    Assert.assertTrue(numTasksInNewInstance <= numTasksInInstanceDown);
+    Assert.assertEquals(editDiff(assignment2, assignment3), numTasksInNewInstance * 2);
   }
 
   private List<DatastreamGroup> generateDatastreams(String namePrefix, int numberOfDatastreams) {
@@ -94,7 +132,7 @@ public class TestBroadcastStrategy {
   public void testBroadcastStrategyDoesntCreateNewTasksWhenCalledSecondTime() {
     String[] instances = new String[]{"instance1", "instance2", "instance3"};
     List<DatastreamGroup> datastreams = generateDatastreams("ds", 5);
-    BroadcastStrategy strategy = new BroadcastStrategy(DEFAULT_MAX_TASKS);
+    StickyMulticastStrategy strategy = new StickyMulticastStrategy(DEFAULT_MAX_TASKS);
     Map<String, Set<DatastreamTask>> assignment =
         strategy.assign(datastreams, Arrays.asList(instances), new HashMap<>());
     Map<String, Set<DatastreamTask>> newAssignment = strategy.assign(datastreams, Arrays.asList(instances), assignment);
@@ -112,7 +150,7 @@ public class TestBroadcastStrategy {
   public void testBroadcastStrategyRemovesDatastreamTasksWhenDatastreamIsDeleted() {
     List<String> instances = Arrays.asList("instance1", "instance2", "instance3");
     List<DatastreamGroup> datastreams = generateDatastreams("ds", 5);
-    BroadcastStrategy strategy = new BroadcastStrategy(DEFAULT_MAX_TASKS);
+    StickyMulticastStrategy strategy = new StickyMulticastStrategy(DEFAULT_MAX_TASKS);
     Map<String, Set<DatastreamTask>> assignment = strategy.assign(datastreams, instances, new HashMap<>());
 
     datastreams.remove(0);
@@ -131,7 +169,7 @@ public class TestBroadcastStrategy {
   public void testBroadcastStrategyCreatesNewTasksOnlyForNewDatastreamWhenDatastreamIsCreated() {
     List<String> instances = Arrays.asList("instance1", "instance2", "instance3");
     List<DatastreamGroup> datastreams = generateDatastreams("ds", 5);
-    BroadcastStrategy strategy = new BroadcastStrategy(DEFAULT_MAX_TASKS);
+    StickyMulticastStrategy strategy = new StickyMulticastStrategy(DEFAULT_MAX_TASKS);
     Map<String, Set<DatastreamTask>> assignment = strategy.assign(datastreams, instances, new HashMap<>());
 
     List<DatastreamGroup> newDatastreams = new ArrayList<>(datastreams);
@@ -155,7 +193,7 @@ public class TestBroadcastStrategy {
     List<String> instances = Arrays.asList("instance1", "instance2", "instance3");
     String instance4 = "instance4";
     List<DatastreamGroup> datastreams = generateDatastreams("ds", 5);
-    BroadcastStrategy strategy = new BroadcastStrategy(DEFAULT_MAX_TASKS);
+    StickyMulticastStrategy strategy = new StickyMulticastStrategy(DEFAULT_MAX_TASKS);
     Map<String, Set<DatastreamTask>> assignment = strategy.assign(datastreams, instances, new HashMap<>());
     List<String> newInstances = new ArrayList<>(instances);
     newInstances.add(instance4);
@@ -170,5 +208,48 @@ public class TestBroadcastStrategy {
     }
 
     Assert.assertEquals(newAssignment.get(instance4).size(), datastreams.size());
+  }
+
+
+
+  private static String assignmentToString(Map<String, Set<DatastreamTask>> assignment) {
+    StringBuilder builder = new StringBuilder();
+    assignment.keySet().stream().sorted().forEach(instance -> {
+      builder.append("Instance: ").append(instance).append(" tasks: {");
+      assignment.get(instance)
+          .stream()
+          .map(DatastreamTask::getTaskPrefix)
+          .sorted()
+          .forEach(taskPrefix -> builder.append(" ").append(taskPrefix).append(','));
+      if (builder.charAt(builder.length() - 1) == ',') {
+        builder.setLength(builder.length() - 1); // Delete the last comma
+      }
+      builder.append("} ");
+    });
+    return builder.toString();
+  }
+
+  private static int editDiff(Map<String, Set<DatastreamTask>> assignment1,
+      Map<String, Set<DatastreamTask>> assignment2) {
+    Set<String> allTasks1 = getFullTaskList(assignment1);
+    Set<String> allTasks2 = getFullTaskList(assignment2);
+
+    Set<String> diff1 = new HashSet<>(allTasks1);
+    diff1.removeAll(allTasks2);
+
+    Set<String> diff2 = new HashSet<>(allTasks2);
+    diff2.removeAll(allTasks1);
+
+    return diff1.size() + diff2.size();
+  }
+
+  private static Set<String> getFullTaskList(Map<String, Set<DatastreamTask>> assignment) {
+    Set<String> allTasks1 = new HashSet<>();
+    for (String instance : assignment.keySet()) {
+      for (DatastreamTask task : assignment.get(instance)) {
+        allTasks1.add(instance + " : " + task.getTaskPrefix());
+      }
+    }
+    return allTasks1;
   }
 }
