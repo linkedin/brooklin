82fdbda62ce643628dc4262d9a7c6ee42c5aec6d
Miguel Sanchez <misanchez@linkedin.com>
2018-01-11 09:52:19 -0800

    Adding utility class FlushlessEventProducerHandler
    The purpose of this class is to keep metrics of inFlight messages for
    each topic partition; and the safe checkpoint processed.

--- datastream-server-api/src/main/java/com/linkedin/datastream/server/Pair.java
+++ datastream-server-api/src/main/java/com/linkedin/datastream/server/Pair.java
@@ -1,5 +1,8 @@
 package com.linkedin.datastream.server;
 
+import java.util.Objects;
+
+
 public class Pair<K, V> {
 
   private final V _value;
@@ -16,5 +19,27 @@ public class Pair<K, V> {
 
   public K getKey() {
     return _key;
+  }
+
+  public boolean equals(Object obj)
+  {
+    if (obj instanceof Pair)
+    {
+      Pair other = (Pair) obj;
+      return  Objects.equals(_key, other._key) &&
+          Objects.equals(_value, other._value);
+    }
+    return false;
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(_key, _value);
+  }
+
+  /*convenient method*/
+  public static <S, T> Pair<S, T> of(S first, T second)
+  {
+    return new Pair<S, T>(first, second);
   }
 }
