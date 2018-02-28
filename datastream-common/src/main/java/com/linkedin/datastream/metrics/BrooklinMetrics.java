a106f6bc2cc3393ddb3ee9924bc7d9a1550dc552
Deepthi Sridharan <desridharan@linkedin.com>
2018-02-28 10:54:58 -0800

    Added DatabaseChunkedReader specific metrics (#411)
    Added metrics specific to the reader. There will be more metrics in the seeder to measure the
    throughput. Since the Reader only provides an iterator, it is not a good place to measure how
    row read throughout. Metrics of interest at the reader layer is time spent in executing queries,
    number of queries executed. Also, if allowed to skip over bad rows, count of such skipped rows.

---
+++ datastream-common/src/main/java/com/linkedin/datastream/metrics/BrooklinMetrics.java
@@ -0,0 +1,51 @@
+package com.linkedin.datastream.metrics;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * Base class for metric categories with support for metric deregistration.
+ * It uses reference counting for deregistering aggregate metrics which can
+ * only happen when all keyed metrics of the same name have been deregistered.
+ */
+public abstract class BrooklinMetrics {
+  protected String _key;
+  protected String _className;
+
+  // Map from a [class,category] to its reference counter
+  private static Map<String, AtomicInteger> _refCounts = new HashMap<>();
+
+  private String getRefKey() {
+    return getClass().getSimpleName() + _className;
+  }
+
+  public BrooklinMetrics(String className, String key) {
+    _className = className;
+    _key = key;
+
+    String refKey = getRefKey();
+    _refCounts.computeIfAbsent(refKey, k -> new AtomicInteger(0));
+    _refCounts.get(refKey).incrementAndGet();
+  }
+
+  /**
+   * Deregister metric from metric registry
+   */
+  public void deregister() {
+    String refKey = getRefKey();
+
+    // Already deregistered?
+    if (!_refCounts.containsKey(refKey)) {
+      return;
+    }
+
+    if (_refCounts.get(refKey).decrementAndGet() == 0) {
+      deregisterAggregates();
+      _refCounts.remove(refKey);
+    }
+  }
+
+  protected void deregisterAggregates() {
+  }
+}
