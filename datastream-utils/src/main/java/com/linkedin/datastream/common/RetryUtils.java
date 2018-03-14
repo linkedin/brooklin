c17906b45d797e73002943b782cb695d8b5d8b33
Srinivasulu Punuru <spunuru@linkedin.com>
2018-03-14 09:57:04 -0700

    Adding logging in the retry utils

--- datastream-utils/src/main/java/com/linkedin/datastream/common/RetryUtils.java
+++ datastream-utils/src/main/java/com/linkedin/datastream/common/RetryUtils.java
@@ -1,33 +1,63 @@
 package com.linkedin.datastream.common;
 
 import java.time.Duration;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.function.BooleanSupplier;
 import java.util.function.Supplier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 
 public class RetryUtils {
 
-  public static <T> T retry(Supplier<T> func, Duration period, Duration timeout) {
-    final List<T> returnValue = new ArrayList<>();
+  private static final Logger LOG = LoggerFactory.getLogger(RetryUtils.class.getName());
 
-    boolean result = PollUtils.poll(() -> {
+  public static <T> T retry(Supplier<T> func, Duration period, Duration timeout) {
+    ExceptionTrackingMethodCaller<T> supplier = new ExceptionTrackingMethodCaller<>(func);
+    boolean result = PollUtils.poll(supplier, period.toMillis(), timeout.toMillis());
+
+    if (!result) {
+      LOG.error("Retries exhausted.", supplier.getLastException());
+      throw new RetriesExhaustedExeption(supplier.getLastException());
+    }
+
+    return supplier.getValue();
+  }
+
+  private static class ExceptionTrackingMethodCaller<U> implements BooleanSupplier {
+    private static final Logger LOG = LoggerFactory.getLogger(ExceptionTrackingMethodCaller.class.getName());
+
+    private final Supplier<U> _func;
+    private U _value;
+
+    public Exception getLastException() {
+      return _lastException;
+    }
+
+    private Exception _lastException;
+
+    public ExceptionTrackingMethodCaller(Supplier<U> func) {
+      _func = func;
+    }
+
+    U getValue() {
+      return _value;
+    }
+
+    @Override
+    public boolean getAsBoolean() {
       try {
-        returnValue.add(func.get());
+        _value = _func.get();
       } catch (Exception e) {
         if (e instanceof RetriableException) {
+          _lastException = e;
+          LOG.info("Method threw a retriable exception.", e);
           return false;
         }
         throw e;
       }
 
       return true;
-    }, period.toMillis(), timeout.toMillis());
-
-    if (!result) {
-      throw new RetriesExhaustedExeption();
     }
-
-    return returnValue.get(0);
   }
 }
+
