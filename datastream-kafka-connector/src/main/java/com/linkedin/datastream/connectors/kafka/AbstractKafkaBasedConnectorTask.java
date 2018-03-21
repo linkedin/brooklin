f8de0e4f63cf6022065bcb1496057f58cd22d334
Celia Kung <ckung@linkedin.com>
2018-03-21 13:31:59 -0700

    Added more diagnostics to show KafkaConsumer TopicPartition assignment

--- datastream-kafka-connector/src/main/java/com/linkedin/datastream/connectors/kafka/AbstractKafkaBasedConnectorTask.java
+++ datastream-kafka-connector/src/main/java/com/linkedin/datastream/connectors/kafka/AbstractKafkaBasedConnectorTask.java
@@ -90,6 +90,7 @@ abstract public class AbstractKafkaBasedConnectorTask implements Runnable, Consu
   protected volatile String _taskName;
   protected DatastreamEventProducer _producer;
   protected Consumer<?, ?> _consumer;
+  protected Set<TopicPartition> _consumerAssignment = Collections.emptySet();
 
   // Datastream task updates that need to be processed
   protected final Set<DatastreamConstants.UpdateType> _taskUpdates = Sets.newConcurrentHashSet();
@@ -507,6 +508,7 @@ abstract public class AbstractKafkaBasedConnectorTask implements Runnable, Consu
     _consumerMetrics.updateRebalanceRate(1);
     _logger.info("Partition ownership assigned for {}.", partitions);
 
+    _consumerAssignment = Sets.newHashSet(partitions);
     // update paused partitions, in case.
     _taskUpdates.add(DatastreamConstants.UpdateType.PAUSE_RESUME_PARTITIONS);
   }
@@ -676,7 +678,8 @@ abstract public class AbstractKafkaBasedConnectorTask implements Runnable, Consu
   }
 
   public KafkaDatastreamStatesResponse getKafkaDatastreamStatesResponse() {
-    return new KafkaDatastreamStatesResponse(_datastreamName, _autoPausedSourcePartitions, _pausedPartitionsConfig);
+    return new KafkaDatastreamStatesResponse(_datastreamName, _autoPausedSourcePartitions, _pausedPartitionsConfig,
+        _consumerAssignment);
   }
 
   /**
--- datastream-kafka-connector/src/main/java/com/linkedin/datastream/connectors/kafka/KafkaDatastreamStatesResponse.java
+++ datastream-kafka-connector/src/main/java/com/linkedin/datastream/connectors/kafka/KafkaDatastreamStatesResponse.java
@@ -5,11 +5,15 @@ import java.util.Map;
 import java.util.Set;
 
 import org.apache.kafka.common.TopicPartition;
+import org.codehaus.jackson.annotate.JsonPropertyOrder;
+import org.codehaus.jackson.map.annotate.JsonSerialize;
+import org.codehaus.jackson.map.ser.ToStringSerializer;
 
 import com.linkedin.datastream.common.JsonUtils;
 import com.linkedin.datastream.server.FlushlessEventProducerHandler;
 
 
+@JsonPropertyOrder({"datastream", "assignedTopicPartitions", "autoPausedPartitions", "manualPausedPartitions", "inFlightMessageCounts"})
 public class KafkaDatastreamStatesResponse {
 
   private String _datastream;
@@ -20,22 +24,26 @@ public class KafkaDatastreamStatesResponse {
   // Map of Kafka topic to set of manually paused partitions
   private Map<String, Set<String>> _manualPausedPartitions;
 
+  // Set of assigned TopicPartitions
+  private Set<TopicPartition> _assignedTopicPartitions;
+
   // Map of source partition to number of in-flight message counts
   Map<FlushlessEventProducerHandler.SourcePartition, Long> _inFlightMessageCounts;
 
   public KafkaDatastreamStatesResponse(String datastream,
       Map<TopicPartition, PausedSourcePartitionMetadata> autoPausedPartitions,
-      Map<String, Set<String>> manualPausedPartitions) {
-    this(datastream, autoPausedPartitions, manualPausedPartitions, Collections.emptyMap());
+      Map<String, Set<String>> manualPausedPartitions, Set<TopicPartition> assignedTopicPartitions) {
+    this(datastream, autoPausedPartitions, manualPausedPartitions, assignedTopicPartitions, Collections.emptyMap());
   }
 
   public KafkaDatastreamStatesResponse(String datastream,
       Map<TopicPartition, PausedSourcePartitionMetadata> autoPausedPartitions,
-      Map<String, Set<String>> manualPausedPartitions,
+      Map<String, Set<String>> manualPausedPartitions, Set<TopicPartition> assignedTopicPartitions,
       Map<FlushlessEventProducerHandler.SourcePartition, Long> inFlightMessageCounts) {
     _datastream = datastream;
     _autoPausedPartitions = autoPausedPartitions;
     _manualPausedPartitions = manualPausedPartitions;
+    _assignedTopicPartitions = assignedTopicPartitions;
     _inFlightMessageCounts = inFlightMessageCounts;
   }
 
@@ -53,6 +61,11 @@ public class KafkaDatastreamStatesResponse {
 
   public Map<FlushlessEventProducerHandler.SourcePartition, Long> getInFlightMessageCounts() {
     return _inFlightMessageCounts;
+  }
+
+  @JsonSerialize(contentUsing = ToStringSerializer.class)
+  public Set<TopicPartition> getAssignedTopicPartitions() {
+    return _assignedTopicPartitions;
   }
 
   public static String toJson(KafkaDatastreamStatesResponse obj) {
