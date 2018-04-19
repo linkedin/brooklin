5c1231e8f0b00b9d36a1a5fe92b71d5f0ff77cc9
Deepthi Sridharan <desridharan@linkedin.com>
2018-04-19 13:32:59 -0700

    Add interruptable perdicate and pull in new interfaces to PollUtils

--- datastream-utils/src/main/java/com/linkedin/datastream/common/PollUtils.java
+++ datastream-utils/src/main/java/com/linkedin/datastream/common/PollUtils.java
@@ -1,15 +1,65 @@
 package com.linkedin.datastream.common;
 
+import java.util.Objects;
 import java.util.Optional;
 import java.util.function.BooleanSupplier;
-import java.util.function.Predicate;
-
 
 /**
  * Utility class for polling an arbitrary condition (boolean/predicate)
  * with timeout/period support.
  */
 public final class PollUtils {
+
+  /**
+   * Interruptable version of {@link java.util.function.Supplier} for use with Poll functions
+   *
+   * <p>There is no requirement that a new or distinct result be returned each
+   * time the supplier is invoked.
+   *
+   * <p>This is a <a href="package-summary.html">functional interface</a>
+   * whose functional method is {@link #get()} which can throw InterruptedException
+   *
+   * @param <T> the type of results supplied by this supplier
+   */
+  @FunctionalInterface
+  public interface InterruptableSupplier<T> {
+    T get() throws InterruptedException;
+  }
+
+  /**
+   * Interruptable version of {@link java.util.function.Predicate} for use with Poll functions
+   *
+   * <p>This is a <a href="package-summary.html">functional interface</a>
+   * whose functional method is {@link #test(Object)} which can throw
+   * InterruptedException
+   *
+   * @param <T> the type of the input to the predicate
+   */
+  @FunctionalInterface
+  public interface InterruptablePredicate<T> {
+
+    boolean test(T t) throws InterruptedException;
+
+    default InterruptablePredicate<T> and(InterruptablePredicate<? super T> other) {
+      Objects.requireNonNull(other);
+      return (t) -> test(t) && other.test(t);
+    }
+
+    default InterruptablePredicate<T> negate() {
+      return (t) -> !test(t);
+    }
+
+
+    default InterruptablePredicate<T> or(InterruptablePredicate<? super T> other) {
+      Objects.requireNonNull(other);
+      return (t) -> test(t) || other.test(t);
+    }
+
+    static <T> InterruptablePredicate<T> isEqual(Object targetRef) {
+      return (null == targetRef) ? Objects::isNull : object -> targetRef.equals(object);
+    }
+  }
+
   /**
    * Blocking poll the condition until it's met or time exceeds timeoutMs
    * @param cond boolean supplier representing the condition
@@ -29,16 +79,16 @@ public final class PollUtils {
    * @param arg input argument for the predicate
    * @return true if condition is met, false otherwise
    */
-  public static <T> boolean poll(Predicate<T> cond, long periodMs, long timeoutMs, T arg) {
+  public static <T> boolean poll(InterruptablePredicate<T> cond, long periodMs, long timeoutMs, T arg) {
     long elapsedMs = 0;
     if (timeoutMs > 0 && periodMs > timeoutMs) {
       return false;
     }
     while (true) {
-      if (cond.test(arg)) {
-        return true;
-      }
       try {
+        if (cond.test(arg)) {
+          return true;
+        }
         Thread.sleep(periodMs);
       } catch (InterruptedException e) {
         break;
@@ -59,7 +109,8 @@ public final class PollUtils {
    * @param interruptableSupplier input argument for the predicate
    * @return an Optional of the result satisfying the condition, empty if it couldn't
    */
-  public static <T> Optional<T> poll(InterruptableSupplier<T> interruptableSupplier, Predicate<T> cond, long periodMs, long timeoutMs) {
+  public static <T> Optional<T> poll(InterruptableSupplier<T> interruptableSupplier, InterruptablePredicate<T> cond,
+      long periodMs, long timeoutMs) {
     long elapsedMs = 0;
     if (periodMs > timeoutMs) {
       return Optional.empty();
@@ -68,13 +119,9 @@ public final class PollUtils {
       T ret;
       try {
         ret = interruptableSupplier.get();
-      } catch (InterruptedException e) {
-        break;
-      }
-      if (cond.test(ret)) {
-        return Optional.of(ret);
-      }
-      try {
+        if (cond.test(ret)) {
+          return Optional.of(ret);
+        }
         Thread.sleep(periodMs);
       } catch (InterruptedException e) {
         break;
