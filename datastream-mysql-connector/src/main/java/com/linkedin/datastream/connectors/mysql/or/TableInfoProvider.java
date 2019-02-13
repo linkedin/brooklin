ff943d3b9a4468e9d1bdbf0856b0491684b02c2d
Shun-ping Chiu <schiu@schiu-mn4.linkedin.biz>
2019-02-13 15:31:06 -0800

    move brooklin-oracle-tb-connector and datastream-mysql-connector out from open source brooklin

--- datastream-mysql-connector/src/main/java/com/linkedin/datastream/connectors/mysql/or/TableInfoProvider.java
+++
@@ -1,9 +0,0 @@
-package com.linkedin.datastream.connectors.mysql.or;
-
-import java.util.List;
-
-
-public interface TableInfoProvider {
-
-  List<ColumnInfo> getColumnList(String dbName, String tableName);
-}
