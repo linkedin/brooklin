cfac0ac79db5d73644bb4f04fae3093ff08f9765
Sonam Mandal <somandal@somandal-ld1.linkedin.biz>
2019-04-25 09:48:17 -0700

    Adding datastream update support for metadata for the NoOpConnector

--- datastream-server-api/src/main/java/com/linkedin/datastream/server/NoOpConnectorFactory.java
+++ datastream-server-api/src/main/java/com/linkedin/datastream/server/NoOpConnectorFactory.java
@@ -7,6 +7,9 @@ package com.linkedin.datastream.server;
 
 import java.util.List;
 import java.util.Properties;
+import java.util.stream.Collectors;
+
+import org.apache.commons.lang3.Validate;
 
 import com.linkedin.datastream.common.Datastream;
 import com.linkedin.datastream.server.api.connector.Connector;
@@ -43,6 +46,55 @@ public class NoOpConnectorFactory implements ConnectorFactory<NoOpConnectorFacto
     @Override
     public void initializeDatastream(Datastream stream, List<Datastream> allDatastreams)
         throws DatastreamValidationException {
+    }
+
+    private boolean doesSourceMatch(Datastream newDatastream, Datastream existingDatastream) {
+      if ((!newDatastream.hasSource() && !existingDatastream.hasSource())
+          || (newDatastream.hasSource() && existingDatastream.hasSource()
+          && newDatastream.getSource().equals(existingDatastream.getSource()))) {
+        return true;
+      }
+      return false;
+    }
+
+    private boolean doesDestinationMatch(Datastream newDatastream, Datastream existingDatastream) {
+      if ((!newDatastream.hasDestination() && !existingDatastream.hasDestination())
+          || (newDatastream.hasDestination() && existingDatastream.hasDestination()
+          && newDatastream.getDestination().equals(existingDatastream.getDestination()))) {
+        return true;
+      }
+      return false;
+    }
+
+    @Override
+    public void validateUpdateDatastreams(List<Datastream> datastreams, List<Datastream> allDatastreams)
+      throws DatastreamValidationException {
+      // Only metadata updates are allowed
+      for (Datastream newDatastream : datastreams) {
+        List<Datastream> existingDatastreams = allDatastreams.stream().filter(ds -> ds.getName()
+            .equals(newDatastream.getName())).collect(Collectors.toList());
+
+        Validate.isTrue(existingDatastreams.size() == 1, "Bad state: Multiple datastreams exist with the "
+            + "same name " + existingDatastreams);
+        Datastream existingDatastream = existingDatastreams.get(0);
+
+        if (newDatastream.equals(existingDatastream)) {
+          continue;
+        }
+
+        if (!doesSourceMatch(newDatastream, existingDatastream)
+            || !doesDestinationMatch(newDatastream, existingDatastream)) {
+          throw new DatastreamValidationException(String.format("Only metadata update is allowed. "
+                  + "Source and destination cannot be updated. Old stream: {%s}; New stream: {%s}", existingDatastream,
+              newDatastream));
+        }
+
+        if (newDatastream.hasMetadata() && existingDatastream.hasMetadata()
+            && newDatastream.getMetadata().equals(existingDatastream.getMetadata())) {
+          throw new DatastreamValidationException(String.format("Only metadata update is allowed. "
+              + "Old stream: {%s}; New stream: {%s}", existingDatastream, newDatastream));
+        }
+      }
     }
   }
 }
---
+++ datastream-server-api/src/test/java/com/linkedin/datastream/server/TestNoOpConnectorFactory.java
@@ -0,0 +1,92 @@
+/**
+ *  Copyright 2019 LinkedIn Corporation. All rights reserved.
+ *  Licensed under the BSD 2-Clause License. See the LICENSE file in the project root for license information.
+ *  See the NOTICE file in the project root for additional information regarding copyright ownership.
+ */
+package com.linkedin.datastream.server;
+
+import java.util.Arrays;
+import java.util.Properties;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.linkedin.data.template.StringMap;
+import com.linkedin.datastream.common.Datastream;
+import com.linkedin.datastream.common.DatastreamDestination;
+import com.linkedin.datastream.common.DatastreamSource;
+import com.linkedin.datastream.server.api.connector.DatastreamValidationException;
+
+/**
+ * Tests for {@link NoOpConnectorFactory}
+ */
+public class TestNoOpConnectorFactory {
+
+  private static Datastream createDatastream(String datastreamName, String transportProviderName) {
+    // create the datastream
+    Datastream datastream = new Datastream();
+    datastream.setName(datastreamName);
+    datastream.setConnectorName("noOpConnector");
+    DatastreamSource source = new DatastreamSource();
+    source.setConnectionString("source_connection_string");
+    datastream.setSource(source);
+    DatastreamDestination destination = new DatastreamDestination();
+    destination.setConnectionString("ListBackedTransportProvider");
+    datastream.setDestination(destination);
+    datastream.setTransportProviderName(transportProviderName);
+    datastream.setMetadata(new StringMap());
+    return datastream;
+  }
+
+  @Test
+  public void testValidateUpdateDatastreams() throws Exception {
+    Properties properties = new Properties();
+
+    NoOpConnectorFactory noOpConnectorFactory = new NoOpConnectorFactory();
+    NoOpConnectorFactory.NoOpConnector connector =
+        noOpConnectorFactory.createConnector("noOpConnector", properties, "cluster");
+
+    Datastream datastream1 = createDatastream("noOp-1", "likafkakac");
+    Datastream datastream2 = createDatastream("noOp-2", "likafkakac");
+
+    // Happy path - no change in either datastream
+    connector.validateUpdateDatastreams(Arrays.asList(datastream1, datastream2), Arrays.asList(datastream1, datastream2));
+
+    // Try to modify source on one of the datastreams
+    Datastream datastream3 = createDatastream("noOp-2", "likafkakac");
+    datastream3.getSource().setConnectionString("source-blah");
+    try {
+      connector.validateUpdateDatastreams(Arrays.asList(datastream1, datastream3), Arrays.asList(datastream1, datastream2));
+      Assert.fail();
+    } catch (DatastreamValidationException e) {
+    }
+
+    // Try to modify destination on one of the datastreams
+    datastream3 = createDatastream("noOp-1", "likafkakac");
+    datastream3.getDestination().setConnectionString("dest-blah");
+    try {
+      connector.validateUpdateDatastreams(Arrays.asList(datastream3, datastream2), Arrays.asList(datastream1, datastream2));
+      Assert.fail();
+    } catch (DatastreamValidationException e) {
+    }
+
+    // Try to modify the transport provider name on one of the datastreams
+    datastream3 = createDatastream("noOp-2", "likafkakactest");
+    try {
+      connector.validateUpdateDatastreams(Arrays.asList(datastream1, datastream3), Arrays.asList(datastream1, datastream2));
+      Assert.fail();
+    } catch (DatastreamValidationException e) {
+    }
+
+    // Happy path - try to modify the metadata on one of the datastreams
+    datastream3 = createDatastream("noOp-1", "likafkakac");
+    datastream3.getMetadata().put("random-metadata-key", "random-metadata-value");
+    connector.validateUpdateDatastreams(Arrays.asList(datastream3, datastream2), Arrays.asList(datastream1, datastream2));
+
+    // Happy path - modify already existing metadata on one of the datastreams
+    datastream2.getMetadata().put("metadata-key", "metadata-value");
+    datastream3 = createDatastream("noOp-2", "likafkakac");
+    datastream3.getMetadata().put("metadata-key", "new-metadata-value");
+    connector.validateUpdateDatastreams(Arrays.asList(datastream1, datastream3), Arrays.asList(datastream1, datastream2));
+  }
+}
