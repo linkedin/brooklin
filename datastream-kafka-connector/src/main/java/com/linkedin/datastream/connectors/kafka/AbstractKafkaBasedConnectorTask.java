613018775d9a034e38212cb4312a506e98510cc7
Shun-ping Chiu <schiu@linkedin.com>
2019-05-28 13:36:52 -0700

    Rewind offset for dropping messages from paused partition (#606)
    * Address a potential issue that a message is ignored due to pause but the partition is not rewound properly

--- datastream-kafka-connector/src/main/java/com/linkedin/datastream/connectors/kafka/AbstractKafkaBasedConnectorTask.java
+++ datastream-kafka-connector/src/main/java/com/linkedin/datastream/connectors/kafka/AbstractKafkaBasedConnectorTask.java
@@ -220,7 +220,9 @@ abstract public class AbstractKafkaBasedConnectorTask implements Runnable, Consu
       for (ConsumerRecord<?, ?> record : records.records(topicPartition)) {
         try {
           if (_autoPausedSourcePartitions.containsKey(topicPartition)) {
-            _logger.warn("Abort sending as {} is auto-paused", topicPartition);
+            _logger.warn("Abort sending as {} is auto-paused, rewind offset", topicPartition);
+            seekToLastCheckpoint(Collections.singleton(topicPartition));
+            break;
           } else {
             DatastreamProducerRecord datastreamProducerRecord = translate(record, readTime);
             int numBytes = record.serializedKeySize() + record.serializedValueSize();
@@ -582,7 +584,7 @@ abstract public class AbstractKafkaBasedConnectorTask implements Runnable, Consu
   /**
    * Seek to the last checkpoint for the given topicPartitions.
    */
-  private void seekToLastCheckpoint(Set<TopicPartition> topicPartitions) {
+  void seekToLastCheckpoint(Set<TopicPartition> topicPartitions) {
     _logger.info("Trying to seek to previous checkpoint for partitions: {}", topicPartitions);
     Map<TopicPartition, OffsetAndMetadata> lastCheckpoint = new HashMap<>();
     Set<TopicPartition> tpWithNoCommits = new HashSet<>();
