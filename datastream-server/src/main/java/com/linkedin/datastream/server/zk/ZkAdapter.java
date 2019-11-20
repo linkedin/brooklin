6be1405ffd5651c862df42f2328af4d496698284
Justin Kaufman <jkaufman@linkedin.com>
2019-11-19 18:55:46 -0800

    Ensure any existing zookeeper sessions are closed before creating a new one (#671)

--- datastream-server/src/main/java/com/linkedin/datastream/server/zk/ZkAdapter.java
+++ datastream-server/src/main/java/com/linkedin/datastream/server/zk/ZkAdapter.java
@@ -201,6 +201,7 @@ public class ZkAdapter {
    * the actions that need to be taken with them, which are implemented in the Coordinator class
    */
   public void connect() {
+    disconnect(); // Guard against leaking an existing zookeeper session
     _zkclient = new ZkClient(_zkServers, _sessionTimeout, _connectionTimeout);
 
     // create a globally unique instance name and create a live instance node in ZooKeeper
