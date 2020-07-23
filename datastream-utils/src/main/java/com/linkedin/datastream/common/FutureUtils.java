5fe498771871e09e32261cd02887179b5b14f42a
Vaibhav Maheshwari <vmaheshw@vmaheshw-mn1.linkedin.biz>
2020-07-23 16:03:03 -0700

    Address review comments

--- datastream-utils/src/main/java/com/linkedin/datastream/common/FutureUtils.java
+++ datastream-utils/src/main/java/com/linkedin/datastream/common/FutureUtils.java
@@ -6,38 +6,68 @@
 package com.linkedin.datastream.common;
 
 import java.time.Duration;
+import java.util.concurrent.CancellationException;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
+import org.apache.commons.lang.NullArgumentException;
 
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
+
+
+/**
+ * Utilities to work with {@link Future}
+ */
 public class FutureUtils {
 
-  private static final ScheduledExecutorService SCHEDULED_EXECUTOR_SERVICE = Executors.newScheduledThreadPool(100);
+  private static final ScheduledExecutorService SCHEDULED_EXECUTOR_SERVICE = Executors.newScheduledThreadPool(4,
+      new ThreadFactoryBuilder().setDaemon(true).setNameFormat("FutureUtils-%d").build());
 
+  private FutureUtils() {
+
+  }
   /**
-   * This function waits till timeout for Future to complete, otherwise, it cancels it.
-   * @param futureToAwait Future to complete/stop
-   * @param timeout time to wait before aborting future
-   * @return CompletableFuture for this operation
+   * Check if {@code future} is done after {@code after}. Otherwise, cancels {@code future}.
+   * It returns {@link CompletableFuture} which should be used directly for light-weight operations to avoid blocking the
+   * executor thread, or call {@link CompletableFuture#supplyAsync(Supplier)} or {@link CompletableFuture#supplyAsync(Supplier, Executor)}
+   * which uses another thread to execute and does not block the main executor thread.
+   * @param future {@code future} to check/cancel
+   * @param after time to wait before aborting {@code future}
+   * @return {@link CompletableFuture} completed with computed result of {@code future}, if {@code future} was done before {@code after}.
+   *         {@link CompletableFuture} completed exceptionally with {@link InterruptedException}, if {@code future} was interrupted.
+   *         {@link CompletableFuture} completed exceptionally with cause of Exception, if {@code future} hit {@link ExecutionException}.
+   *         {@link CompletableFuture} cancelled, if {@code future} thread was cancelled.
+   * @throws NullArgumentException {@link NullArgumentException} is Thrown if future is Null
    */
-  public static CompletableFuture<?> awaitFutureOrCancel(Future<?> futureToAwait, Duration timeout) {
-    CompletableFuture<?> result = new CompletableFuture<>();
+  public static <T> CompletableFuture<T> getIfDoneOrCancel(Future<T> future, Duration after) throws
+                                                                                             NullArgumentException {
+    if (future == null) {
+      throw new NullArgumentException("future");
+    }
+
+    CompletableFuture<T> result = new CompletableFuture<>();
 
     SCHEDULED_EXECUTOR_SERVICE.schedule(() -> {
-      if ((futureToAwait == null) || futureToAwait.isDone()) {
-        result.complete(null);
-      } else {
-        boolean cancel = futureToAwait.cancel(true);
-        if (!cancel) {
-          result.cancel(true);
-        } else {
-          result.complete(null);
+      if (future.isDone()) {
+        try {
+          T t = future.get();
+          result.complete(t);
+        } catch (InterruptedException e) {
+          result.completeExceptionally(e);
+        } catch (ExecutionException e) {
+          result.completeExceptionally(e.getCause());
+        } catch (CancellationException e) {
+          result.cancel(false);
         }
+      } else {
+        future.cancel(true);
+        result.cancel(false);
       }
-    }, timeout.toMillis(), TimeUnit.MILLISECONDS);
+    }, after.toMillis(), TimeUnit.MILLISECONDS);
     return result;
   }
 }
--- datastream-utils/src/test/java/com/linkedin/datastream/common/TestFutureUtils.java
+++ datastream-utils/src/test/java/com/linkedin/datastream/common/TestFutureUtils.java
@@ -8,6 +8,7 @@ package com.linkedin.datastream.common;
 import java.time.Duration;
 import java.util.concurrent.CancellationException;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -19,29 +20,33 @@ import org.testng.annotations.Test;
 public class TestFutureUtils {
 
   @Test
-  public void withinFutureTimesOutAfterDuration() {
+  public void withinFutureTimesOutAfterDuration() throws ExecutionException, InterruptedException {
     runAndVerifyTimeoutFuture(10000, 10, true);
     runAndVerifyTimeoutFuture(5, 10, false);
   }
 
-  private void runAndVerifyTimeoutFuture(long futureRunTime, long timeout, boolean expectedException) {
-    CompletableFuture<?> future = CompletableFuture.runAsync(() -> {
+  private void runAndVerifyTimeoutFuture(long futureRunTime, long timeout, boolean expectedException)
+      throws ExecutionException, InterruptedException {
+    CompletableFuture<Integer> future = CompletableFuture.supplyAsync(() -> {
       try {
         Thread.sleep(futureRunTime);
+        return 10;
       } catch (Exception ex) {
         throw new RuntimeException();
       }
     });
 
     boolean exception = false;
-    CompletableFuture<?> result = FutureUtils.awaitFutureOrCancel(future, Duration.ofMillis(timeout));
-    result.join();
+    FutureUtils.getIfDoneOrCancel(future, Duration.ofMillis(timeout));
     try {
-       future.join();
+      future.join();
     } catch (Exception ex) {
       Assert.assertTrue(ex instanceof CancellationException);
       exception = true;
     }
     Assert.assertEquals(exception, expectedException);
+    if (!exception) {
+      Assert.assertEquals((Integer) 10, future.get());
+    }
   }
 }
