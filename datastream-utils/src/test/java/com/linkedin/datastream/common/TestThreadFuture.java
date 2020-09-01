87dc4b425019b4045d3ddc6764b5b98287ee2c54
Ahmed Abdul Hamid <ahabdulh@linkedin.com>
2020-09-01 08:28:07 -0700

    Update year in copyright header

--- datastream-utils/src/test/java/com/linkedin/datastream/common/TestThreadFuture.java
+++ datastream-utils/src/test/java/com/linkedin/datastream/common/TestThreadFuture.java
@@ -1,5 +1,5 @@
 /**
- *  Copyright 2019 LinkedIn Corporation. All rights reserved.
+ *  Copyright 2020 LinkedIn Corporation. All rights reserved.
  *  Licensed under the BSD 2-Clause License. See the LICENSE file in the project root for license information.
  *  See the NOTICE file in the project root for additional information regarding copyright ownership.
  */
