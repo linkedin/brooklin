bd1cfeca070aecb5dbb705b8418ac66e0a419fc3
Vaibhav Maheshwari <vmaheshwari@linkedin.com>
2020-10-14 14:23:54 -0700

    Add ref count support in DynamicMetricsManager for registeration/deregistration. (#765)
    DynamicMetricManager currently returns the existing metrics if exists or new metrics when trying to register metrics based on the full key name. So, if the same metrics is registered by 2 class/threads, DynamicMetricsManager is not aware of the ref count. So, when anyone of the class/thread de-register the shared metrics, the metrics is de-registered and no longer emitted.
    Currently BrooklinMetrics has a ref-count check for aggregate metrics to make sure the de-registration happens when all the registration references have de-registered. This is how aggregate metrics was working. The assumption may be that we will not see this conflict in any other case.
    
    For KafkaMirrorMakerConnectorTask, the metrics are registered using datastream name and not the task name, so there is a possibility of the conflict. So, in the PR, I'm adding the ref count logic in DynamicMetricsManager itself. I'm also cleaning up BrooklinMetrics which has the logic for aggregate metrics, as we don't need to special case it. I'm keeping the existing interfaces for backward compatibility and can be cleaned up in future if required.

--- datastream-common/src/main/java/com/linkedin/datastream/metrics/BrooklinMetrics.java
+++ datastream-common/src/main/java/com/linkedin/datastream/metrics/BrooklinMetrics.java
@@ -5,19 +5,11 @@
  */
 package com.linkedin.datastream.metrics;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.concurrent.atomic.AtomicInteger;
-
-
 /**
  * Base class for metric categories with support for metric deregistration.
- * It uses reference counting for deregistering aggregate metrics which can
- * only happen when all keyed metrics of the same name have been deregistered.
+ * Underlying DynamicMetricsManager will take care of maintaining ref-counts.
  */
 public abstract class BrooklinMetrics {
-  // Map from a [class,category] to its reference counter
-  private static final Map<String, AtomicInteger> REF_COUNTS = new HashMap<>();
 
   protected final String _key;
   protected final String _className;
@@ -30,31 +22,13 @@ public abstract class BrooklinMetrics {
   public BrooklinMetrics(String className, String key) {
     _className = className;
     _key = key;
-
-    String refKey = getRefKey();
-    REF_COUNTS.computeIfAbsent(refKey, k -> new AtomicInteger(0));
-    REF_COUNTS.get(refKey).incrementAndGet();
-  }
-
-  private String getRefKey() {
-    return getClass().getSimpleName() + _className;
   }
 
   /**
    * Deregister metric from metric registry
    */
   public void deregister() {
-    String refKey = getRefKey();
-
-    // Already deregistered?
-    if (!REF_COUNTS.containsKey(refKey)) {
-      return;
-    }
-
-    if (REF_COUNTS.get(refKey).decrementAndGet() == 0) {
-      deregisterAggregates();
-      REF_COUNTS.remove(refKey);
-    }
+    deregisterAggregates();
   }
 
   protected void deregisterAggregates() {
