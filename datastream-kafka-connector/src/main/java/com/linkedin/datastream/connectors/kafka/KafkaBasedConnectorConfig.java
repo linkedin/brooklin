422e25dc3487ac8fdc98da5d2a99636a4ca4bd8c
Sonam Mandal <somandal@linkedin.com>
2020-10-16 15:25:01 -0700

    Add a configurable feature to include the datastream name in the consumer client.id for BMM

--- datastream-kafka-connector/src/main/java/com/linkedin/datastream/connectors/kafka/KafkaBasedConnectorConfig.java
+++ datastream-kafka-connector/src/main/java/com/linkedin/datastream/connectors/kafka/KafkaBasedConnectorConfig.java
@@ -32,6 +32,7 @@ public class KafkaBasedConnectorConfig {
   public static final String CONFIG_PAUSE_PARTITION_ON_ERROR = "pausePartitionOnError";
   public static final String CONFIG_PAUSE_ERROR_PARTITION_DURATION_MILLIS = "pauseErrorPartitionDurationMs";
   public static final String ENABLE_ADDITIONAL_METRICS = "enableAdditionalMetrics";
+  public static final String INCLUDE_DATASTREAM_NAME_IN_CONSUMER_CLIENT_ID = "includeDatastreamNameInConsumerClientId";
   public static final String DAEMON_THREAD_INTERVAL_SECONDS = "daemonThreadIntervalInSeconds";
   public static final String NON_GOOD_STATE_THRESHOLD_MILLIS = "nonGoodStateThresholdMs";
   public static final String PROCESSING_DELAY_LOG_THRESHOLD_MILLIS = "processingDelayLogThreshold";
@@ -48,6 +49,7 @@ public class KafkaBasedConnectorConfig {
   private static final long DEFAULT_PROCESSING_DELAY_LOG_THRESHOLD_MILLIS = Duration.ofMinutes(1).toMillis();
   private static final long DEFAULT_COMMIT_TIMEOUT_MILLIS = Duration.ofSeconds(30).toMillis();
   private static final boolean DEFAULT_ENABLE_ADDITIONAL_METRICS = Boolean.TRUE;
+  private static final boolean DEFAULT_INCLUDE_DATASTREAM_NAME_IN_CONSUMER_CLIENT_ID = Boolean.FALSE;
 
   private final Properties _consumerProps;
   private final VerifiableProperties _connectorProps;
@@ -64,6 +66,7 @@ public class KafkaBasedConnectorConfig {
   private final Duration _pauseErrorPartitionDuration;
   private final long _processingDelayLogThresholdMillis;
   private final boolean _enableAdditionalMetrics;
+  private final boolean _includeDatastreamNameInConsumerClientId;
 
   private final int _daemonThreadIntervalSeconds;
   private final long _nonGoodStateThresholdMillis;
@@ -103,6 +106,8 @@ public class KafkaBasedConnectorConfig {
             DEFAULT_PROCESSING_DELAY_LOG_THRESHOLD_MILLIS);
     _enableAdditionalMetrics = verifiableProperties.getBoolean(ENABLE_ADDITIONAL_METRICS,
         DEFAULT_ENABLE_ADDITIONAL_METRICS);
+    _includeDatastreamNameInConsumerClientId = verifiableProperties.getBoolean(
+        INCLUDE_DATASTREAM_NAME_IN_CONSUMER_CLIENT_ID, DEFAULT_INCLUDE_DATASTREAM_NAME_IN_CONSUMER_CLIENT_ID);
     _enablePartitionAssignment = verifiableProperties.getBoolean(ENABLE_PARTITION_ASSIGNMENT, Boolean.FALSE);
 
     String factory =
@@ -154,6 +159,10 @@ public class KafkaBasedConnectorConfig {
 
   public boolean getEnableAdditionalMetrics() {
     return _enableAdditionalMetrics;
+  }
+
+  public boolean getIncludeDatastreamNameInConsumerClientId() {
+    return _includeDatastreamNameInConsumerClientId;
   }
 
   /**
