7fa12cc2bdcea4af2b47569c2412c788f4a12999
Sonam Mandal <somandal@linkedin.com>
2020-10-19 09:16:16 -0700

    Add a configurable feature to include the datastream name in the consumer client.id for BMM (#768)

--- datastream-kafka-connector/src/main/java/com/linkedin/datastream/connectors/kafka/mirrormaker/KafkaMirrorMakerConnectorTask.java
+++ datastream-kafka-connector/src/main/java/com/linkedin/datastream/connectors/kafka/mirrormaker/KafkaMirrorMakerConnectorTask.java
@@ -62,7 +62,6 @@ import com.linkedin.datastream.server.FlushlessEventProducerHandler;
 import com.linkedin.datastream.server.api.transport.SendCallback;
 
 
-
 /**
  * KafkaMirrorMakerConnectorTask consumes from Kafka using regular expression pattern subscription. This means that the
  * task is consuming from multiple topics at once. When a new topic falls into the subscription, the task should
@@ -87,6 +86,8 @@ public class KafkaMirrorMakerConnectorTask extends AbstractKafkaBasedConnectorTa
   private static final String KAFKA_ORIGIN_OFFSET = "kafka-origin-offset";
   private static final Duration LOCK_ACQUIRE_TIMEOUT = Duration.ofMinutes(3);
   private static final String TASK_LOCK_ACQUIRE_ERROR_RATE = "taskLockAcquireErrorRate";
+
+  private static final String DATASTREAM_NAME_BASED_CLIENT_ID_FORMAT = "%s-%s";
 
   // constants for flushless mode and flow control
   protected static final String CONFIG_MAX_IN_FLIGHT_MSGS_THRESHOLD = "maxInFlightMessagesThreshold";
@@ -119,6 +120,9 @@ public class KafkaMirrorMakerConnectorTask extends AbstractKafkaBasedConnectorTa
   private final boolean _isFlushlessModeEnabled;
   private final boolean _isIdentityMirroringEnabled;
   private final boolean _enablePartitionAssignment;
+  // If enabled, this appends the datastream name to the consumer client.id. This can be useful to differentiate
+  // among Kafka consumer client metrics for different datastreams.
+  private final boolean _includeDatastreamNameInConsumerClientId;
   private final String _destinationTopicPrefix;
   private FlushlessEventProducerHandler<Long> _flushlessProducer = null;
   private boolean _flowControlEnabled = false;
@@ -145,6 +149,7 @@ public class KafkaMirrorMakerConnectorTask extends AbstractKafkaBasedConnectorTa
     _connectorName = connectorName;
     _isIdentityMirroringEnabled = KafkaMirrorMakerDatastreamMetadata.isIdentityPartitioningEnabled(_datastream);
     _enablePartitionAssignment = config.getEnablePartitionAssignment();
+    _includeDatastreamNameInConsumerClientId = config.getIncludeDatastreamNameInConsumerClientId();
     _destinationTopicPrefix = task.getDatastreams().get(0).getMetadata()
         .getOrDefault(DatastreamMetadataConstants.DESTINATION_TOPIC_PREFIX, DEFAULT_DESTINATION_TOPIC_PREFIX);
     _dynamicMetricsManager = DynamicMetricsManager.getInstance();
@@ -200,7 +205,13 @@ public class KafkaMirrorMakerConnectorTask extends AbstractKafkaBasedConnectorTa
     properties.putIfAbsent(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, CONSUMER_AUTO_OFFSET_RESET_CONFIG_EARLIEST);
     properties.putIfAbsent(ConsumerConfig.GROUP_ID_CONFIG,
         getKafkaGroupId(_datastreamTask, _groupIdConstructor, _consumerMetrics, LOG));
-    LOG.info("Creating Kafka consumer for task {} with properties {}", _datastreamTask, properties);
+    if (_includeDatastreamNameInConsumerClientId) {
+      String clientId = properties.getProperty(ConsumerConfig.CLIENT_ID_CONFIG, "");
+      properties.put(ConsumerConfig.CLIENT_ID_CONFIG,
+          String.format(DATASTREAM_NAME_BASED_CLIENT_ID_FORMAT, clientId, _datastreamName));
+    }
+    LOG.info("Creating Kafka consumer for task {} with properties {}, include datastream name in client.id: {}",
+        _datastreamTask, properties, _includeDatastreamNameInConsumerClientId);
     return _consumerFactory.createConsumer(properties);
   }
 
