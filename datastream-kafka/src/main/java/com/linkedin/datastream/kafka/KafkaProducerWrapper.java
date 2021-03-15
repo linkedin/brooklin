8b7fbd1749b6da40e3cacea744751619fbd19917
Vaibhav Maheshwari <vmaheshwari@linkedin.com>
2021-03-15 12:00:59 -0700

    Refactor KafkaProducerWrapper flush() call (#815)
    * Refactor flush call in kafkaproducerwrapper
    
    Co-authored-by: Vaibhav Maheshwari <vmaheshw@vmaheshw-mn1.linkedin.biz>

--- datastream-kafka/src/main/java/com/linkedin/datastream/kafka/KafkaProducerWrapper.java
+++ datastream-kafka/src/main/java/com/linkedin/datastream/kafka/KafkaProducerWrapper.java
@@ -397,43 +397,20 @@ class KafkaProducerWrapper<K, V> {
     if (producer != null) {
       try {
         producer.flush(_producerFlushTimeoutMs, TimeUnit.MILLISECONDS);
-      } catch (IllegalStateException e) {
-        _log.warn("Hitting IllegalStateException during kafka producer flush.", e);
-        boolean throwException = false;
-        try {
-          _producerLock.lock();
-          if (producer == _kafkaProducer) {
-            if (_closeInProgress) {
-              try {
-                _log.info("Waiting for the producer to close.");
-                throwException = !(_waitOnProducerClose.await(_producerCloseTimeoutMs, TimeUnit.MILLISECONDS));
-                _log.info("Producer close completed for the kafka producer");
-              } catch (InterruptedException ex) {
-                throwException = true;
-                _log.warn("Hit InterruptedException while waiting for the producer to close", ex);
-              }
-            } else {
-              // any other wrapped exception
-              throwException = true;
-              shutdownProducer(true);
-            }
-          } else {
-            _log.info("Kafka producer is already closed.");
-          }
-        } finally {
-          _producerLock.unlock();
-          if (throwException) {
-            throw e;
-          }
-        }
+        _log.info("Flush completed for the producer, closeInProgress: {}", _closeInProgress);
       } catch (Exception e) {
+        _log.warn("Hitting Exception during kafka producer flush.", e);
         // The KafkaProducer object should not be reused on an interrupted/timed out flush. To be safe, we try to
         // close the producer on any exception.
         try {
           _producerLock.lock();
           if (producer == _kafkaProducer) {
-            _log.warn("Kafka producer flush may be interrupted/timed out, closing producer {}.", producer);
-            shutdownProducer(true);
+            if (_closeInProgress) {
+              _log.warn("Kafka producer flush may be interrupted/timed out, producer {} close is in progress.", producer);
+            } else {
+              _log.warn("Kafka producer flush may be interrupted/timed out, closing producer {}.", producer);
+              shutdownProducer(true);
+            }
           } else {
             _log.warn("Kafka producer flush may be interrupted/timed out, producer {} already closed.", producer);
           }
