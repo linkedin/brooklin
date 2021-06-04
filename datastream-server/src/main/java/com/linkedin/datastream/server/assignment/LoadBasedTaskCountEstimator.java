03803ea95b35d2a05af7b36df2cefb934abf0056
Jhora Zakaryan <jzakaryan@linkedin.com>
2021-06-04 13:55:17 -0700

    Implemented load based task count estimator (#829)

--- datastream-server/src/main/java/com/linkedin/datastream/server/assignment/LoadBasedTaskCountEstimator.java
+++ datastream-server/src/main/java/com/linkedin/datastream/server/assignment/LoadBasedTaskCountEstimator.java
@@ -5,17 +5,44 @@
  */
 package com.linkedin.datastream.server.assignment;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
-import org.apache.commons.lang.NotImplementedException;
+import org.apache.commons.lang3.Validate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.linkedin.datastream.server.ClusterThroughputInfo;
+import com.linkedin.datastream.server.PartitionThroughputInfo;
 
 
 /**
  * Estimates the minimum number of tasks for a datastream based on per-partition throughput information
+ * <p>
+ *   The reason for having a separate class for task count estimation is the intent to refactor other task count
+ *   estimation mechanisms (e.g. elastic task count estimation) and make them all implement the same interface.
+ * </p>
  */
 public class LoadBasedTaskCountEstimator {
+  private static final Logger LOG = LoggerFactory.getLogger(LoadBasedTaskCountEstimator.class.getName());
+  // TODO Move these to config class
+  private static final int BYTES_IN_KB_RATE_DEFAULT = 5;
+  private static final int MESSAGES_IN_RATE_DEFAULT = 5;
+
+  private final int _taskCapacityMBps;
+  private final int _taskCapacityUtilizationPct;
+
+  /**
+   * Creates an instance of {@link LoadBasedTaskCountEstimator}
+   * @param taskCapacityMBps Task capacity in MB/sec
+   * @param taskCapacityUtilizationPct Task capacity utilization percentage
+   */
+  public LoadBasedTaskCountEstimator(int taskCapacityMBps, int taskCapacityUtilizationPct) {
+    _taskCapacityMBps = taskCapacityMBps;
+    _taskCapacityUtilizationPct = taskCapacityUtilizationPct;
+  }
 
   /**
    * Gets the estimated number of tasks based on per-partition throughput information.
@@ -27,6 +54,30 @@ public class LoadBasedTaskCountEstimator {
    */
   public int getTaskCount(ClusterThroughputInfo throughputInfo, List<String> assignedPartitions,
       List<String> unassignedPartitions) {
-    throw new NotImplementedException();
+    Validate.notNull(throughputInfo, "null throughputInfo");
+    Validate.notNull(assignedPartitions, "null assignedPartitions");
+    Validate.notNull(unassignedPartitions, "null unassignedPartitions");
+    LOG.info("Assigned partitions: {}", assignedPartitions);
+    LOG.info("Unassigned partitions: {}", unassignedPartitions);
+
+    Map<String, PartitionThroughputInfo> throughputMap = throughputInfo.getPartitionInfoMap();
+    Set<String> allPartitions = new HashSet<>(assignedPartitions);
+    allPartitions.addAll(unassignedPartitions);
+
+    PartitionThroughputInfo defaultThroughputInfo = new PartitionThroughputInfo(BYTES_IN_KB_RATE_DEFAULT,
+        MESSAGES_IN_RATE_DEFAULT, "");
+    // total throughput in KB/sec
+    int totalThroughput = allPartitions.stream()
+        .map(p -> throughputMap.getOrDefault(p, defaultThroughputInfo))
+        .mapToInt(PartitionThroughputInfo::getBytesInKBRate)
+        .sum();
+    LOG.info("Total throughput in all {} partitions: {}KB/sec", allPartitions.size(), totalThroughput);
+
+    double taskCapacityUtilizationCoefficient = _taskCapacityUtilizationPct / 100.0;
+    int taskCountEstimate = (int) Math.ceil((double) totalThroughput /
+        (_taskCapacityMBps * 1024 * taskCapacityUtilizationCoefficient));
+    taskCountEstimate = Math.min(allPartitions.size(), taskCountEstimate);
+    LOG.info("Estimated number of tasks required to handle the throughput: {}", taskCountEstimate);
+    return taskCountEstimate;
   }
 }
---
+++ datastream-server/src/test/java/com/linkedin/datastream/server/TestLoadBasedTaskCountEstimator.java
@@ -0,0 +1,100 @@
+/**
+ *  Copyright 2021 LinkedIn Corporation. All rights reserved.
+ *  Licensed under the BSD 2-Clause License. See the LICENSE file in the project root for license information.
+ *  See the NOTICE file in the project root for additional information regarding copyright ownership.
+ */
+package com.linkedin.datastream.server;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import com.linkedin.datastream.server.assignment.LoadBasedTaskCountEstimator;
+import com.linkedin.datastream.server.providers.FileBasedPartitionThroughputProvider;
+
+
+/**
+ * Tests for {@link LoadBasedTaskCountEstimator}
+ */
+public class TestLoadBasedTaskCountEstimator {
+  private static final String THROUGHPUT_FILE_NAME = "partitionThroughput.json";
+  private static final int TASK_CAPACITY_MBPS = 4;
+  private static final int TASK_CAPACITY_UTILIZATION_PCT = 90;
+
+  private FileBasedPartitionThroughputProvider _provider;
+
+  /**
+   * Test class initialization code
+   */
+  @BeforeClass
+  public void setup() {
+    _provider = new FileBasedPartitionThroughputProvider(THROUGHPUT_FILE_NAME);
+  }
+
+  @Test
+  public void emptyAssignmentReturnsZeroTasksTest() {
+    ClusterThroughputInfo throughputInfo = _provider.getThroughputInfo("pizza");
+    List<String> assignedPartitions = Collections.emptyList();
+    List<String> unassignedPartitions = Collections.emptyList();
+    LoadBasedTaskCountEstimator estimator = new LoadBasedTaskCountEstimator(TASK_CAPACITY_MBPS,
+        TASK_CAPACITY_UTILIZATION_PCT);
+    int taskCount = estimator.getTaskCount(throughputInfo, assignedPartitions, unassignedPartitions);
+    Assert.assertEquals(taskCount, 0);
+  }
+
+  @Test
+  public void lowThroughputAssignmentReturnsOneTaskTest() {
+    ClusterThroughputInfo throughputInfo = _provider.getThroughputInfo("pizza");
+    List<String> assignedPartitions = new ArrayList<>();
+    assignedPartitions.add("Pepperoni-1");
+    List<String> unassignedPartitions = Collections.emptyList();
+    LoadBasedTaskCountEstimator estimator = new LoadBasedTaskCountEstimator(TASK_CAPACITY_MBPS,
+        TASK_CAPACITY_UTILIZATION_PCT);
+    int taskCount = estimator.getTaskCount(throughputInfo, assignedPartitions, unassignedPartitions);
+    Assert.assertEquals(taskCount, 1);
+  }
+
+  @Test
+  public void highThroughputAssignmentTest() {
+    ClusterThroughputInfo throughputInfo = _provider.getThroughputInfo("ice-cream");
+    List<String> assignedPartitions = Collections.emptyList();
+    List<String> unassignedPartitions = new ArrayList<>(throughputInfo.getPartitionInfoMap().keySet());
+    LoadBasedTaskCountEstimator estimator = new LoadBasedTaskCountEstimator(TASK_CAPACITY_MBPS,
+        TASK_CAPACITY_UTILIZATION_PCT);
+    int taskCount = estimator.getTaskCount(throughputInfo, assignedPartitions, unassignedPartitions);
+
+    int throughputSum = throughputInfo.getPartitionInfoMap().values().stream().mapToInt(
+        PartitionThroughputInfo::getBytesInKBRate).sum();
+    double taskCapacityCoefficient = TASK_CAPACITY_UTILIZATION_PCT / 100.0;
+    Assert.assertTrue(taskCount >=
+        throughputSum / (TASK_CAPACITY_MBPS * taskCapacityCoefficient * 1024));
+  }
+
+  @Test
+  public void highThroughputAssignmentTest2() {
+    ClusterThroughputInfo throughputInfo = _provider.getThroughputInfo("donut");
+    List<String> assignedPartitions = Collections.emptyList();
+    List<String> unassignedPartitions = new ArrayList<>(throughputInfo.getPartitionInfoMap().keySet());
+    LoadBasedTaskCountEstimator estimator = new LoadBasedTaskCountEstimator(TASK_CAPACITY_MBPS,
+        TASK_CAPACITY_UTILIZATION_PCT);
+    int taskCount = estimator.getTaskCount(throughputInfo, assignedPartitions, unassignedPartitions);
+    Assert.assertEquals(taskCount, unassignedPartitions.size());
+  }
+
+  @Test
+  public void partitionsHaveDefaultWeightTest() {
+    ClusterThroughputInfo throughputInfo = new ClusterThroughputInfo("dummy", new HashMap<>());
+    List<String> assignedPartitions = Collections.emptyList();
+    List<String> unassignedPartitions = Arrays.asList("P1", "P2");
+    LoadBasedTaskCountEstimator estimator = new LoadBasedTaskCountEstimator(TASK_CAPACITY_MBPS,
+        TASK_CAPACITY_UTILIZATION_PCT);
+    int taskCount = estimator.getTaskCount(throughputInfo, assignedPartitions, unassignedPartitions);
+    Assert.assertTrue(taskCount > 0);
+  }
+}
