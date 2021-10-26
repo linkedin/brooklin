998a371b9ebb5ae1c43d156a9cf0399d95d1a967
Jhora Zakaryan <jzakaryan@linkedin.com>
2021-10-26 10:41:46 -0700

    Fixed checkstyle issue with the imports

--- datastream-server/src/test/java/com/linkedin/datastream/server/assignment/TestLoadBasedPartitionAssignmentStrategy.java
+++ datastream-server/src/test/java/com/linkedin/datastream/server/assignment/TestLoadBasedPartitionAssignmentStrategy.java
@@ -8,13 +8,13 @@ package com.linkedin.datastream.server.assignment;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.function.Predicate;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
+import java.util.function.Predicate;
 
 import org.apache.commons.lang3.StringUtils;
 import org.mockito.Mockito;
