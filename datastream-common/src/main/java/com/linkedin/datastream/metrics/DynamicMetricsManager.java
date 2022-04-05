83463dd47234aeaafdee98d3e7badad99092f5a0
Ryanne Dolan <rdolan@linkedin.com>
2022-04-05 13:55:20 -0500

    Drop createOrUpdateGauge for now

--- datastream-common/src/main/java/com/linkedin/datastream/metrics/DynamicMetricsManager.java
+++ datastream-common/src/main/java/com/linkedin/datastream/metrics/DynamicMetricsManager.java
@@ -330,37 +330,6 @@ public class DynamicMetricsManager {
   }
 
   /**
-   * Updates the Gauge (or creates it if it does not exist) for the specified key/metricName pair to the given value.
-   * A Gauge used in this way should not have an explicit supplier.
-   * @param classSimpleName the simple name of the underlying class
-   * @param key the key (i.e. topic or partition) for the metric
-   * @param metricName the metric name
-   * @param value the new value
-   */
-  @SuppressWarnings("unchecked")
-  public <T> void createOrUpdateGauge(String classSimpleName, String key, String metricName, T value) {
-    validateArguments(classSimpleName, metricName);
-    // create and register the metric if it does not exist
-    ResettableGauge<T> gauge = (ResettableGauge<T>) checkCache(classSimpleName, key, metricName).orElseGet(() -> {
-      ResettableGauge<T> newGauge = (ResettableGauge<T>) _metricRegistry.gauge(
-        MetricRegistry.name(classSimpleName, key, metricName), () -> new ResettableGauge<>(() -> value));
-      updateCache(classSimpleName, key, metricName, newGauge);
-      return newGauge;
-    });
-    gauge.setSupplier(() -> value);
-  }
-
-  /**
-   * Updates the Gauge (or creates it if it does not exist) for the specified metricName to the given value.
-   * @param classSimpleName the simple name of the underlying class
-   * @param metricName the metric name
-   * @param value the new value
-   */
-  public <T> void createOrUpdateGauge(String classSimpleName, String metricName, T value) {
-    createOrUpdateGauge(classSimpleName, null, metricName, value);
-  }
-
-  /**
    * Update the histogram (or creates it if it does not exist) for the specified key/metricName pair by the given value.
    * If the histogram does not exist, create one using {@link SlidingTimeWindowArrayReservoir} with the specified window
    * time in ms. This can be useful for certain metrics that don't want to use the
