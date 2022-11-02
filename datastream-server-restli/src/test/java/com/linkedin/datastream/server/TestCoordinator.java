6c9dc2164ea7c31da045a3e5e42bba9471de6cc4
Harshil Shukla <harshiljshukla@gmail.com>
2022-11-01 18:02:33 -0700

    Update tests

--- datastream-server-restli/src/test/java/com/linkedin/datastream/server/TestCoordinator.java
+++ datastream-server-restli/src/test/java/com/linkedin/datastream/server/TestCoordinator.java
@@ -1399,17 +1399,17 @@ public class TestCoordinator {
     datastreamResources.update(datastream.getName(), datastream);
 
     assertConnectorReceiveDatastreamUpdate(connector1, datastream);
-    PollUtils.poll(() -> connector1.getPostDSStatechangeActionInvokeCount() == 2, 200, WAIT_TIMEOUT_MS);
-    Assert.assertEquals(connector1.getPostDSStatechangeActionInvokeCount(), 2);
-    PollUtils.poll(() -> connector2.getPostDSStatechangeActionInvokeCount() == 1, 200, WAIT_TIMEOUT_MS);
-    Assert.assertEquals(connector2.getPostDSStatechangeActionInvokeCount(), 1);
+    Assert.assertTrue(PollUtils.poll(() -> connector1.getPostDSStatechangeActionInvokeCount() == 2, 200,
+        WAIT_TIMEOUT_MS));
+    Assert.assertTrue(PollUtils.poll(() -> connector2.getPostDSStatechangeActionInvokeCount() == 1, 200,
+        WAIT_TIMEOUT_MS));
 
     LOG.info("Deleting datastream: {}", datastream);
     datastreamResources.delete(datastream.getName());
 
     // post datastream state chance method for DELETE should be invoked only for a leader instance
-    PollUtils.poll(() -> connector1.getPostDSStatechangeActionInvokeCount() == 3, 200, WAIT_TIMEOUT_MS);
-    Assert.assertEquals(connector1.getPostDSStatechangeActionInvokeCount(), 3);
+    Assert.assertTrue(PollUtils.poll(() -> connector1.getPostDSStatechangeActionInvokeCount() == 3, 200,
+        WAIT_TIMEOUT_MS));
     Assert.assertEquals(connector2.getPostDSStatechangeActionInvokeCount(), 1);
 
     coordinator1.stop();
@@ -1653,7 +1653,8 @@ public class TestCoordinator {
 
     // connector1 is assigned on instance1 which is a leader
     // and post datastream create/delete method will be invoked only for a leader instance
-    Assert.assertEquals(connector1.getPostDSStatechangeActionInvokeCount(), concurrencyLevel);
+    Assert.assertTrue(PollUtils.poll(() -> connector1.getPostDSStatechangeActionInvokeCount() == concurrencyLevel,
+        200, WAIT_TIMEOUT_MS));
     Assert.assertEquals(connector2.getPostDSStatechangeActionInvokeCount(), 0);
 
     instance1.stop();
@@ -2012,7 +2013,8 @@ public class TestCoordinator {
 
     // connector1 is assigned on instance1 which is a leader
     // and post datastream create/update/delete or state chance method will be invoked only for a leader instance
-    Assert.assertEquals(connector1.getPostDSStatechangeActionInvokeCount(), dsCount);
+    Assert.assertTrue(PollUtils.poll(() -> connector1.getPostDSStatechangeActionInvokeCount() == dsCount, 200,
+        WAIT_TIMEOUT_MS));
     Assert.assertEquals(connector2.getPostDSStatechangeActionInvokeCount(), 0);
     Assert.assertEquals(connector3.getPostDSStatechangeActionInvokeCount(), 0);
 
@@ -2345,7 +2347,7 @@ public class TestCoordinator {
     PollUtils.poll(() -> connector1.getAssignmentCount() == 12, 200, WAIT_TIMEOUT_MS);
     int childrenCount = zkClient.countChildren(errorPath);
     Assert.assertTrue(childrenCount <= 10);
-    Assert.assertEquals(connector1.getPostDSCount(), 12);
+    Assert.assertTrue(PollUtils.poll(() -> connector1.getPostDSCount() == 12, 200, WAIT_TIMEOUT_MS));
     //
     // clean up
     //
@@ -2480,14 +2482,16 @@ public class TestCoordinator {
     // Make sure connector has received the assignment (timeout in 30 seconds)
     assertConnectorAssignment(setup._connector, 30000, datastreamName);
     // post datastream create/update/delete or state chance method should be called
-    Assert.assertEquals(setup._connector.getPostDSStatechangeActionInvokeCount(), 1);
+    Assert.assertTrue(PollUtils.poll(() -> setup._connector.getPostDSStatechangeActionInvokeCount() == 1,
+        200, WAIT_TIMEOUT_MS));
 
     // Delete the data stream and verify proper cleanup
     UpdateResponse deleteResponse = setup._resource.delete(stream.getName());
     Assert.assertEquals(deleteResponse.getStatus(), HttpStatus.S_200_OK);
     assertConnectorAssignment(setup._connector, 30000);
     // post datastream create/update/delete or state chance method should be called
-    Assert.assertEquals(setup._connector.getPostDSStatechangeActionInvokeCount(), 2);
+    Assert.assertTrue(PollUtils.poll(() -> setup._connector.getPostDSStatechangeActionInvokeCount() == 2,
+        200, WAIT_TIMEOUT_MS));
   }
 
   @Test
@@ -2623,7 +2627,8 @@ public class TestCoordinator {
     Assert.assertEquals(createResponse.getStatus(), HttpStatus.S_201_CREATED);
 
     // post datastream create/update/delete or state chance method should be called
-    Assert.assertEquals(setup._connector.getPostDSStatechangeActionInvokeCount(), 1);
+    Assert.assertTrue(PollUtils.poll(() -> setup._connector.getPostDSStatechangeActionInvokeCount() == 1, 200,
+        WAIT_TIMEOUT_MS));
 
     // Creating a stream2 which should trigger stream1 to be deleted
     createResponse = setup._resource.create(streams[1]);
@@ -2642,6 +2647,8 @@ public class TestCoordinator {
     }, 200, Duration.ofSeconds(30).toMillis());
 
     // post datastream create/update/delete or state chance method should be called
+    Assert.assertTrue(PollUtils.poll(() -> setup._connector.getPostDSStatechangeActionInvokeCount() == 2, 200,
+        WAIT_TIMEOUT_MS));
     Assert.assertEquals(setup._connector.getPostDSStatechangeActionInvokeCount(), 2);
   }
 
